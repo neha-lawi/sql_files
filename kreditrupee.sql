88d4d71e74aea563c1876d784ada931cbae42751
neha-lawi <neha.mer@mobavenue.com>
2025-01-14 13:51:46 +0530

    quid,kreditrupee

--- kreditrupee.sql
+++ kreditrupee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2024 at 05:41 AM
+-- Generation Time: Jan 14, 2025 at 09:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
